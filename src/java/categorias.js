931f75985bafbbdbc9d08980ae1d04dde98adfe8
Pierre <sotopierre1@gmail.com>
2025-03-15 18:03:21 -0300

    se ha eliminado ciertos comandos que no se ocuparan

--- src/java/categorias.js
+++ src/java/categorias.js
@@ -1,9 +1,9 @@
 function showCategory(category) {
-    // Ocultar todas las categorías
+
     var categories = document.querySelectorAll('.category');
     categories.forEach(function(cat) {
         cat.style.display = 'none';
     });
-    // Mostrar la categoría seleccionada
+
     document.getElementById(category).style.display = 'block';
 }
